d46b43f3f9956aec079ca8a45535d8f392c75f52
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-06-22 22:18:35 -0500

    CanAcitvate Guards y localStorage

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,24 +1,58 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements  CanLoad {
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
-  constructor(private authService: AuthService){}
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    console.log('CanActivate', this.authService.auth.id ? true : false);
+    console.log('ruta', route);
+    console.log('RouterStateSnapshot', state);
+    // return (this.authService.auth.id)? true: false;
+    return this.authService.verificaAutenticacion().pipe(
+      tap((tieneAcceso) => {
+        if (!tieneAcceso) {
+          this.router.navigate(['./auth']);
+        }
+      })
+    );
+  }
+
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean{
-      console.log('CanLoad', (this.authService.auth.id)? true: false);
-      console.log('ruta', route);
-      console.log('segements', segments);
-    return (this.authService.auth.id)? true: false;
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    console.log('CanLoad', this.authService.auth.id ? true : false);
+    console.log('ruta', route);
+    console.log('segements', segments);
+    // return (this.authService.auth.id)? true: false;
+    return this.authService.verificaAutenticacion().pipe(
+      tap((tieneAcceso) => {
+        if (!tieneAcceso) {
+          this.router.navigate(['./auth']);
+        }
+      })
+    );
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,26 +2,42 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Auth } from '../interfaces/auth.interface';
-import { tap } from 'rxjs';
+import { tap, Observable, of, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private _auth: Auth| undefined;
-  get auth(){
-    return {...this._auth};
+  private _auth: Auth | undefined;
+  get auth() {
+    return { ...this._auth };
   }
-  private base_url = environment.base_url;
-  constructor(private http: HttpClient) { }
 
-    login(){
+  private base_url = environment.base_url;
+  constructor(private http: HttpClient) {}
+
+  login() {
     return this.http.get<Auth>(`${this.base_url}/usuarios/1`).pipe(
-      tap(resp => this._auth = resp )
+      tap((resp) => (this._auth = resp)),
+      tap((resp) => localStorage.setItem('token', resp.id.toString()))
     );
   }
 
-  logout(){
+  verificaAutenticacion(): Observable<boolean>{
+    if(!localStorage.getItem('token')){
+      return of(false);
+    }
+    return this.http.get<Auth>(`${this.base_url}/usuarios/1`).pipe(
+      map(aut => {
+        this._auth = aut;
+        console.log('VAlor del auth', aut);
+        return true
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('token');
     this._auth = undefined;
   }
 }
